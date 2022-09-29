e6ce579f38e6b412d7c7c93e48988d41814b011e
bilal1999-cyber <bilal_aslam@iinv.tech>
2022-09-29 14:38:02 +0500

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,7 +9,7 @@ const Form = () => {
         email: "", 
         phoneNumber: "", 
         date: "",
-        gender: "",
+        gender: "Male",
         username: "",
         password: ""
     }
@@ -35,12 +35,13 @@ const Form = () => {
   } else {
     marginLeft = ""
   }
-  
+
   function increment() {
     setCount(prevState => prevState + 1)
     if (count > 2) {
       setCount(3)
     }
+
   }
   function decrement() {
     setCount(prevState => prevState - 1)
@@ -48,11 +49,19 @@ const Form = () => {
       setCount(0)
     }
   }
+
+  /// REGEX AND VALIDATION ///
+
+  const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/
+  const nameValidation = formData.firstName.length > 2 && formData.lastName.length > 2 
+
+  //
+
   return (
     <>
       <div className='p-7 overflow-hidden form pt-10 lg:pt-11'>
         <div>
-          <h1 className='text-center text-4xl font-semibold'>Signup Form</h1>
+          <h1 className='text-center text-3xl font-semibold'>Signup Form</h1>
         </div>
         <div className='flex mt-10 font-medium justify-center gap-6 align-center'>
           <p>Name</p>
@@ -61,11 +70,11 @@ const Form = () => {
           <p>Submit</p>
         </div>
         <div className="progress__bar gap-1 mt-2 flex justify-center">
-          <div className={`${count > 0 && "active"} progess_block`}>1</div>
+          <div className={`${count > 0 && "active box-shadow-none"} progess_block`}>1</div>
           <div className={`${count > 0 && "active__line"} progress_line`}></div>
-          <div className={`${count > 1 && "active"} progess_block`}>2</div>
+          <div className={`${count > 1 && "active box-shadow-none"} progess_block`}>2</div>
           <div className={`${count > 1 && "active__line"} progress_line`}></div>
-          <div className={`${count > 2 && "active"} progess_block`}>3</div>
+          <div className={`${count > 2 && "active box-shadow-none"} progess_block`}>3</div>
           <div className={`${count > 2 && "active__line"} progress_line`}></div>
           <div className={`${count > 3 && "active"} progess_block`}>4</div>
         </div>
@@ -78,33 +87,40 @@ const Form = () => {
                 <input 
                   tabIndex="-1" 
                   pattern="[a-z]{3,}" 
-                  className='pl-2 mb-3 w-full py-2 border' 
+                  className='pl-2 mb-3 w-full py-2 border ph' 
                   type="text" 
                   name="firstName"
                   onChange={handleChange}
                   value={formData.firstName}
+                  placeholder="First Name (min 3 characters)"
                   id=""
                 />
                 <span className='mb-1 block'>Last Name</span>
                 <input 
                   tabIndex="-1"
-                  pattern="[a-z]{3,}" 
-                  className='pl-2 w-full py-2 border border-l-slate-200'
+                  pattern="[a-z]{3,}"
+                  className='pl-2 w-full py-2 border border-l-slate-200 ph'
                   type="text"
                   name="lastName"
                   onChange={handleChange}
                   value={formData.lastName}
+                  placeholder="Last Name (min 3 characters)"
                   id="" 
                 />
                 <div className='flex justify-between mt-9'>
                   <button 
-                    disabled={formData.firstName && formData.lastName ? false : true}
-                    onClick={increment} 
+                    onClick={() => {                   
+                      if (nameValidation) {
+                        increment();
+                      } else {
+                        alert("Please fill in the data")
+                      }
+                    }} 
                     tabIndex="-1" 
                     type="button" 
                     className='py-2 btn w-full bg_pink text-xl font-medium text-white rounded'
                     style={{
-                      cursor: formData.firstName && formData.lastName ? "pointer" : "not-allowed",
+                      cursor: nameValidation ?  "pointer" : "not-allowed",
                     }}
                     >
                       Next
@@ -144,13 +160,14 @@ const Form = () => {
                     Previous
                   </button>
                   <button 
-                    disabled={formData.phoneNumber && formData.email ? false : true}
-                    onClick={increment} 
+                    disabled={formData.phoneNumber.length > 5 && emailRegex.test(formData.email) ? false : true}
+                    onClick={increment}
+                    placeholder="Phone number (min 6 characters)" 
                     type="button" 
                     className='btn_pri bg_pink text-xl font-medium text-white rounded'  
                     tabIndex="-1"
                     style={{
-                      cursor: formData.phoneNumber && formData.email ? "pointer" : "not-allowed",
+                      cursor: formData.phoneNumber.length > 5 && emailRegex.test(formData.email) ? "pointer" : "not-allowed"
                     }}
                     >
                     Next
@@ -172,7 +189,7 @@ const Form = () => {
                 <span className='mb-1 block'  tabIndex="-1">Gender</span>
                 <select  
                   tabIndex="-1" 
-                  className='pl-2 bg-white w-full py-2 border' 
+                  className='pl-2 text-gray-500 bg-white w-full py-2 border' 
                   name="gender"
                   onChange={handleChange}
                   value={formData.gender}
@@ -208,38 +225,42 @@ const Form = () => {
                 <span className='mb-1 block'>Username</span>
                 <input 
                   tabIndex="-1" 
-                  className='pl-2 mb-3 w-full py-2 border' 
-                  type="text" 
+                  className='ph pl-2 mb-3 w-full py-2 border' 
+                  type="text"
+                  pattern=".[a-zA-Z0-9-]{7,}" 
                   name="username"
                   onChange={handleChange}
                   value={formData.username}
+                  placeholder="Username"
                   id="" 
                 />
                 <span className='mb-1 block'>Password</span>
                 <input 
                   tabIndex="-1" 
-                  className='pl-2 w-full py-2 border border-l-slate-200' 
-                  type="phone-1" 
+                  className='ph pl-2 w-full py-2 border border-l-slate-200' 
+                  type="text"
+                  pattern=".[a-zA-Z0-9-]{8,}" 
                   name="password"
+                  placeholder="Password"
                   onChange={handleChange}
                   value={formData.password}
                   id="" 
                 />
                 <div className='flex justify-between mt-9'>
                   <button 
-                    onClick={decrement} 
+                    onClick={decrement}
                     type="button" 
                     className='btn_pri bg_pink text-xl font-medium text-white rounded'  
                     tabIndex="-1">
                     Previous
                   </button>
                   <button 
-                    disabled={formData.username && formData.password ? false : true}
+                    disabled={formData.username.length > 6 && formData.password.length > 7 ? false : true}
                     type="submit" 
                     className='btn_pri bg_pink text-xl font-medium text-white rounded'  
                     tabIndex="-1"
                     style={{
-                      cursor: formData.username && formData.password ? "pointer" : "not-allowed"
+                      cursor: formData.username.length > 6 && formData.password.length > 7 ? "pointer" : "not-allowed"
                     }}
                     >
                     Submit
